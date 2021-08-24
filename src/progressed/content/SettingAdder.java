a907fdb420a0683ec7a327367aca827686a2be15
MEEP of Faith <meepmatthew@gmail.com>
2021-08-24 02:45:35 -0700

    LOUD Fart

--- src/progressed/content/SettingAdder.java
+++ src/progressed/content/SettingAdder.java
@@ -1,7 +1,9 @@
 package progressed.content;
 
+import arc.*;
 import arc.scene.*;
 import arc.scene.ui.layout.*;
+import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.ui.*;
 import mindustry.ui.dialogs.*;
@@ -22,9 +24,15 @@ public class SettingAdder{
         progm.sliderPref("pm-swordopacity", 100, 20, 100, 5, s -> s + "%");
         progm.sliderPref("pm-strobespeed", 3, 1, 20, 1, s -> PMUtls.stringsFixed(s / 2f));
         progm.checkPref("pm-tesla-range", true);
-        progm.checkPref("pm-farting", false, b -> Sounds.wind3.play());
+        progm.checkPref("pm-farting", false, b -> Sounds.wind3.play(Core.settings.getInt("sfxvol"))); //haha yes LOUD FART
 
         dialog.cont.center().add(progm);
+
+        Events.on(ResizeEvent.class, event -> {
+           if(dialog.isShown() && Core.scene.getDialog() == dialog){
+               dialog.updateScrollFocus();
+           }
+        });
 
         ui.settings.shown(() -> {
             Table settingUi = (Table)((Group)((Group)(ui.settings.getChildren().get(1))).getChildren().get(0)).getChildren().get(0); //This looks so stupid lol
