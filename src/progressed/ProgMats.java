d4fabe919d1dfb1a8e95c327c4733cadaacd93a4
MEEP of Faith <meepmatthew@gmail.com>
2021-10-27 18:31:05 -0700

    Better contributor code

--- src/progressed/ProgMats.java
+++ src/progressed/ProgMats.java
@@ -2,7 +2,6 @@ package progressed;
 
 import arc.*;
 import arc.func.*;
-import arc.util.*;
 import mindustry.ctype.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
@@ -13,6 +12,7 @@ import progressed.content.bullets.*;
 import progressed.graphics.*;
 import progressed.util.*;
 
+import static arc.Core.*;
 import static mindustry.Vars.*;
 
 public class ProgMats extends Mod{
@@ -35,7 +35,7 @@ public class ProgMats extends Mod{
         Events.on(ClientLoadEvent.class, e -> settingAdder.init());
 
         // Load all assets once they're added into Vars.tree
-        Events.on(FileTreeInitEvent.class, e -> Core.app.post(() -> {
+        Events.on(FileTreeInitEvent.class, e -> app.post(() -> {
             PMShaders.load();
             PMSounds.load();
         }));
@@ -50,16 +50,25 @@ public class ProgMats extends Mod{
             renderer.maxZoom = 24f; //Get a closer look at yourself
 
             LoadedMod progM = mods.locateMod("prog-mats");
-            Func<String, String> stringf = value -> Core.bundle.get("mod." + value);
+            Func<String, String> getModBundle = value -> bundle.get("mod." + value);
 
             progM.meta.displayName = "[#FCC21B]" + progM.meta.displayName + "[]";
-            progM.meta.author = stringf.get(progM.meta.name + ".author");
-            progM.meta.description = stringf.get(progM.meta.name + ".description");
+            progM.meta.description = getModBundle.get(progM.meta.name + ".description");
+
+
+            StringBuilder contributors = new StringBuilder(getModBundle.get(progM.meta.name + ".author"));
+            contributors.append("\n\n").append("[#FCC21B]Contributors:[]");
+            int i = 0;
+            while(bundle.has("mod." + progM.meta.name + "-contributor." + i)){
+                contributors.append("\n        ").append(getModBundle.get(progM.meta.name + "-contributor." + i));
+                i++;
+            }
+            progM.meta.author = contributors.toString();
 
             Events.on(ClientLoadEvent.class, e -> {
                 PMUtls.godHood(PMUnitTypes.everythingUnit);
 
-                if(Core.settings.getBool("pm-farting")){
+                if(settings.getBool("pm-farting")){
                     content.blocks().each(b -> {
                         b.destroySound = Sounds.wind3;
                     });
@@ -77,21 +86,7 @@ public class ProgMats extends Mod{
         for(ContentList list : pmContent){
             list.load();
 
-            Log.info("@: Loaded content list: @", getClass().getSimpleName(), list.getClass().getSimpleName());
+            //Log.info("@: Loaded content list: @", getClass().getSimpleName(), list.getClass().getSimpleName());
         }
-    }
-
-    public static void print(Object... args){
-        StringBuilder builder = new StringBuilder();
-        if(args == null){
-            builder.append("null");
-        }else{
-            for(int i = 0; i < args.length; i++){
-                builder.append(args[i]);
-                if(i < args.length - 1) builder.append(", ");
-            }
-        }
-
-        Log.info("&lc&fb[PM]&fr @", builder.toString());
     }
 }
