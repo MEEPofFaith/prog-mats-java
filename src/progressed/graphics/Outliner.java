1de4d5a94168938b02bb9e28dbca1b179af786e6
MEEP of Faith <meepmatthew@gmail.com>
2022-01-08 00:31:49 -0800

    Outdatedmindy

--- src/progressed/graphics/Outliner.java
+++ src/progressed/graphics/Outliner.java
@@ -10,34 +10,13 @@ import mindustry.graphics.*;
 public class Outliner{
     /**
      * Outlines a given textureRegion. Run in createIcons.
-     * @author Sunny
      * */
-    public static void outlineRegion(MultiPacker packer, TextureRegion tex, Color outlineColor, String name, int radius){
+    public static void outlineRegion(MultiPacker packer, TextureRegion tex, Color outlineColor, String name, int outlineRadius){
         if(tex == null) return;
         PixmapRegion region = Core.atlas.getPixmap(tex);
-        Pixmap out = new Pixmap(region.width, region.height);
-        Color color = new Color();
-        for(int x = 0; x < region.width; x++){
-            for(int y = 0; y < region.height; y++){
-
-                region.get(x, y, color);
-                out.set(x, y, color);
-                if(color.a < 1f){
-                    boolean found = false;
-                    outer:
-                    for(int rx = -radius; rx <= radius; rx++){
-                        for(int ry = -radius; ry <= radius; ry++){
-                            if(Structs.inBounds(rx + x, ry + y, region.width, region.height) && Mathf.within(rx, ry, radius + 0.5f) && color.set(region.get(rx + x, ry + y)).a > 0.01f){
-                                found = true;
-                                break outer;
-                            }
-                        }
-                    }
-                    if(found){
-                        out.set(x, y, outlineColor);
-                    }
-                }
-            }
+        Pixmap out = Pixmaps.outline(region, outlineColor, outlineRadius);
+        if(Core.settings.getBool("linear", true)){
+            Pixmaps.bleed(out);
         }
         packer.add(MultiPacker.PageType.main, name, out);
     }
@@ -48,7 +27,6 @@ public class Outliner{
 
     /**
      * Outlines a list of regions. Run in createIcons.
-     * @author Sunny
      * */
     public static void outlineRegions(MultiPacker packer, TextureRegion[] textures, Color outlineColor, String name, int radius){
         for(int i = 0; i < textures.length; i++){
