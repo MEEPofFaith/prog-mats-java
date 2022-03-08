0d9e15652dda407d380c1574e3917f272a1364dd
MEEP of Faith <meepmatthew@gmail.com>
2022-03-08 13:11:36 -0800

    Might be a bad idea to make content not always load
    Use `visibility` instead.

--- src/progressed/content/PMPlanets.java
+++ src/progressed/content/PMPlanets.java
@@ -11,27 +11,26 @@ public class PMPlanets{
     theGrandPoo;
 
     public static void load(){
-        if(ProgMats.farting()){
-            // *wipes tear* It's beautiful.
-            theGrandPoo = new Planet("the-grand-poo", Planets.serpulo, 0.25f){{
-                bloom = true;
-                accessible = false;
-                orbitRadius = 2;
-                lightColor = Color.valueOf("6e340a");
-                atmosphereColor = Color.valueOf("663712");
+        // *wipes tear* It's beautiful.
+        theGrandPoo = new Planet("the-grand-poo", Planets.serpulo, 0.25f){{
+            bloom = true;
+            accessible = false;
+            visible = ProgMats.farting();
+            orbitRadius = 2;
+            lightColor = Color.valueOf("6e340a");
+            atmosphereColor = Color.valueOf("663712");
 
-                meshLoader = () -> new SunMesh(
-                    this, 4,
-                    5, 0.3, 1.7, 1.2, 1,
-                    1.1f,
-                    Color.valueOf("52230e"),
-                    Color.valueOf("472502"),
-                    lightColor,
-                    atmosphereColor,
-                    Color.valueOf("d6af11"),
-                    Color.valueOf("b5a528")
-                );
-            }};
-        }
+            meshLoader = () -> new SunMesh(
+                this, 4,
+                5, 0.3, 1.7, 1.2, 1,
+                1.1f,
+                Color.valueOf("52230e"),
+                Color.valueOf("472502"),
+                lightColor,
+                atmosphereColor,
+                Color.valueOf("d6af11"),
+                Color.valueOf("b5a528")
+            );
+        }};
     }
 }
