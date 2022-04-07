06d1511b3fe8fd6bd1d82bbcca292be862975df6
MEEP of Faith <meepmatthew@gmail.com>
2022-04-06 19:59:30 -0700

    Hydroconveyor Recolor
    Even more hydroconveyor sprite touch-ups wooooooooooo-*dies*

--- src/progressed/world/blocks/distribution/CoveredConveyor.java
+++ src/progressed/world/blocks/distribution/CoveredConveyor.java
@@ -2,19 +2,17 @@ package progressed.world.blocks.distribution;
 
 import arc.*;
 import arc.graphics.g2d.*;
-import arc.math.*;
-import arc.math.geom.*;
 import arc.util.*;
+import mindustry.*;
 import mindustry.entities.units.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
-import mindustry.type.*;
 import mindustry.world.blocks.distribution.*;
-
-import static mindustry.Vars.*;
+import progressed.util.*;
 
 public class CoveredConveyor extends Conveyor{
     public TextureRegion[] coverRegions = new TextureRegion[5];
+    public TextureRegion inputRegion, outputRegion;
 
     public CoveredConveyor(String name){
         super(name);
@@ -33,6 +31,8 @@ public class CoveredConveyor extends Conveyor{
         for(int i = 0; i < 5; i++){
             coverRegions[i] = Core.atlas.find(name + "-cover-" + i);
         }
+        inputRegion = Core.atlas.find(name + "-cover-in");
+        outputRegion = Core.atlas.find(name + "-cover-out");
     }
 
     @Override
@@ -48,17 +48,40 @@ public class CoveredConveyor extends Conveyor{
     }
 
     public class FloatingConveyorBuild extends ConveyorBuild{
+        public boolean backCap, leftCap, rightCap, frontCap;
+
         @Override
         public void draw(){
             super.draw();
 
             Draw.z(Layer.block - 0.08f);
-            Draw.rect(coverRegions[blendbits], x, y, coverRegions[blendbits].width / 4f * blendsclx, coverRegions[blendbits].height / 4f * blendscly, rotation * 90);
+            Draw.rect(coverRegions[blendbits], x, y, Vars.tilesize * blendsclx, Vars.tilesize * blendscly, rotation * 90);
+
+            if(frontCap) Draw.rect(outputRegion, x, y, rotdeg());
+            if(!backCap) Draw.rect(inputRegion, x, y, rotdeg());
+            if(leftCap) Draw.rect(inputRegion, x, y, rotdeg() - 90f);
+            if(rightCap) Draw.rect(inputRegion, x, y, rotdeg() + 90f);
         }
 
         @Override
         public void unitOn(Unit unit){
             //There is a cover, can't slide on this thing
+        }
+
+        @Override
+        public void onProximityUpdate(){
+            super.onProximityUpdate();
+
+            frontCap = nextc == null || block != nextc.block;
+
+            Building backB = back();
+            backCap = blendbits == 1 || PMUtls.relativeDirection(backB, this) == 0 && backB.block == block;
+
+            Building leftB = left();
+            leftCap = blendbits != 0 && PMUtls.relativeDirection(leftB, this) == 0 && leftB.block != block;
+
+            Building rightB = right();
+            rightCap = blendbits != 0 && PMUtls.relativeDirection(rightB, this) == 0 && rightB.block != block;
         }
     }
 }
