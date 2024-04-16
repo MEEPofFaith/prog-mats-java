0fa7bc626d48d604909a4dd9f8abdd2efa21c0ef
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2024-04-15 23:44:00 -0700

    Fix having multiple space part draws at the same time break everything
    Cursed shenanigans

--- src/progressed/entities/part/SpacePart.java
+++ src/progressed/entities/part/SpacePart.java
@@ -3,13 +3,17 @@ package progressed.entities.part;
 import arc.*;
 import arc.graphics.*;
 import arc.graphics.g2d.*;
+import arc.struct.*;
 import arc.util.*;
 import mindustry.entities.part.*;
+import mindustry.game.EventType.*;
 import progressed.graphics.*;
 
 import static mindustry.Vars.*;
 
 public class SpacePart extends DrawPart{
+    private static ObjectMap<Float, Seq<SpaceData>> draws;
+
     /** Appended to unit/weapon/block name and drawn. */
     public String suffix = "";
     /** Overrides suffix if set. */
@@ -19,16 +23,32 @@ public class SpacePart extends DrawPart{
 
     public SpacePart(String suffix){
         this.suffix = suffix;
+
+        if(draws == null){
+            draws = new ObjectMap<>();
+
+            Events.run(Trigger.drawOver, () -> {
+                for(Float layer : draws.keys()){
+                    Seq<SpaceData> datas = draws.get(layer);
+                    Draw.draw(layer, () -> {
+                        renderer.effectBuffer.begin(Color.clear);
+                        for(SpaceData data : datas){
+                            Draw.rect(data.region, data.x, data.y, data.rot);
+                        }
+                        renderer.effectBuffer.end();
+                        renderer.effectBuffer.blit(PMShaders.smallSpaceShader);
+                    });
+                }
+                draws.clear();
+            });
+        }
     }
 
     @Override
     public void draw(PartParams params){
-        Draw.draw(layer > 0 ? layer : Draw.z(), () -> {
-            renderer.effectBuffer.begin(Color.clear);
-            Draw.rect(region, params.x, params.y, params.rotation - 90f);
-            renderer.effectBuffer.end();
-            renderer.effectBuffer.blit(PMShaders.smallSpaceShader);
-        });
+        float l = layer > 0 ? layer : Draw.z();
+        Seq<SpaceData> data = draws.get(l, Seq::new);
+        data.add(new SpaceData(params.x, params.y, params.rotation - 90f, region));
     }
 
     @Override
@@ -36,5 +56,17 @@ public class SpacePart extends DrawPart{
         String realName = this.name == null ? name + suffix : this.name;
 
         region = Core.atlas.find(realName);
+    }
+
+    private static class SpaceData{
+        float x, y, rot;
+        TextureRegion region;
+
+        private SpaceData(float x, float y, float rot, TextureRegion region){
+            this.x = x;
+            this.y = y;
+            this.rot = rot;
+            this.region = region;
+        }
     }
 }
