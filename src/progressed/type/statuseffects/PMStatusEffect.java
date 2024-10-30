99bb1773f7c07f45680ad93e9d05755bf5fbcc7d
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2024-10-30 16:49:32 -0700

    Yoink status sprite generation

--- src/progressed/type/statuseffects/PMStatusEffect.java
+++ src/progressed/type/statuseffects/PMStatusEffect.java
@@ -1,22 +1,28 @@
 package progressed.type.statuseffects;
 
+import arc.*;
 import arc.graphics.*;
 import mindustry.graphics.*;
+import mindustry.graphics.MultiPacker.*;
 import mindustry.type.*;
-import progressed.graphics.*;
 
 public class PMStatusEffect extends StatusEffect{
-    /** Outlined icon color.*/
-    public Color outlineColor = PMPal.outline;
-
     public PMStatusEffect(String name){
         super(name);
+        outline = true;
     }
 
     @Override
-    public void createIcons(MultiPacker packer){
-        Outliner.outlineRegion(packer, fullIcon, outlineColor, name, 3);
+    public void createIcons(MultiPacker packer){ //Yoink from generator.
+        //color image
+        Pixmap base = Core.atlas.getPixmap(uiIcon).crop();
+        Pixmap tint = base;
+        base.each((x, y) -> tint.setRaw(x, y, Color.muli(tint.getRaw(x, y), color.rgba())));
 
-        super.createIcons(packer);
+        //outline the image
+        Pixmap container = new Pixmap(tint.width + 6, tint.height + 6);
+        container.draw(base, 3, 3, true);
+        base = container.outline(Pal.gray, 3);
+        packer.add(PageType.ui, name, base);
     }
 }
